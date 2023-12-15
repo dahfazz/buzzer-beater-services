9b7376f5de9e8961add05506e3f37c560b8424ca
fabien zibi <fabien.zibi@gmail.com>
2023-12-15 12:32:46 +0100

    feat: crosses with ppg

--- cross.service.js
+++ cross.service.js
@@ -9,16 +9,26 @@ const getCross = async (key1, key2) => {
   const result2 = await axios.get(`https://www.basketball-reference.com/teams/${key2.toUpperCase()}/players.html`);
 
   let $ = cheerio.load(result1.data);
-  $('#div_franchise_register a').each((_, tag) => {
-    PLAYERS1.push($(tag).text())
+  $('#div_franchise_register tr').each((_, line) => {
+    PLAYERS1.push({
+      name: $(line).find('a').eq(0).text(),
+      ppg: parseInt($(line).find('td[data-stat="pts_per_g"]').eq(0).text(), 10),
+    })
   });
 
   $ = cheerio.load(result2.data);
-  $('#div_franchise_register a').each((_, tag) => {
-    PLAYERS2.push($(tag).text())
+  $('#div_franchise_register tr').each((_, line) => {
+    PLAYERS2.push({
+      name: $(line).find('a').eq(0).text(),
+      ppg: parseInt($(line).find('td[data-stat="pts_per_g"]').eq(0).text(), 10),
+    })
   });
 
-  return PLAYERS1.filter(player => PLAYERS2.includes(player))
+  return PLAYERS1.filter(player => {
+    const find = PLAYERS2.find(_p => _p.name === player.name);
+
+    return player.name && !!find;
+  }).sort((a, b) => a.ppg > b.ppg ? 1 : -1)
 }
 
 module.exports = { getCross };
